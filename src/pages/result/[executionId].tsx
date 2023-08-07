ecea30277a8fba4869fe77e0c433d589bcc0fede
James Stuart Milne <jstuartmilne@gmail.com>
2023-08-07 20:59:49 -0300

    feat: add generate another button

--- src/pages/result/[executionId].tsx
+++ src/pages/result/[executionId].tsx
@@ -2,15 +2,14 @@ import { Stack, Box } from "@mui/material";
 import { GetServerSidePropsContext } from "next";
 import MainLayout from "@/components/MainLayout";
 import { MovieSummary } from "@/components/containers/MovieSummary";
-import { MainTitle, SubText2 } from "@/components/elements/texts/Typographys";
+import { MainTitle } from "@/components/elements/texts/Typographys";
+import { PrimaryButton } from "@/components/elements/buttons/Buttons";
 import { getExecution } from "@/serverhelpers/utils";
-import {
-  WorkflowStatus,
-} from "@io-orkes/conductor-javascript";
+import { WorkflowStatus } from "@io-orkes/conductor-javascript";
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const executionId = context.params?.executionId as string;
-  const executionAndStatus = getExecution(executionId); 
+  const executionAndStatus = getExecution(executionId);
 
   return {
     props: executionAndStatus,
@@ -39,9 +38,12 @@ export default function Result(props: Props) {
             alignItems={"center"}
             direction={"row"}
           >
-            <MovieSummary genre="Action" summary={props.workflowStatus?.output?.text}/>
-
+            <MovieSummary
+              genre="Action"
+              summary={props.workflowStatus?.output?.text}
+            />
           </Stack>
+          <PrimaryButton href="/">Generate Another</PrimaryButton>
         </Stack>
       </Box>
     </MainLayout>
